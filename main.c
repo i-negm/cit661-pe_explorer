5907a6f4ea20e34509bb07c29de75710379a5fa0
Islam NEGM <islam.g.negm@gmail.com>
2022-03-24 16:01:08 -0700

    Add windows PE struct and prepare the parsers

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int argc, void* argv[]) {
+#include <windows.h>
+#include <winnt.h>
+
+int main (int argc, char** argv) {
     
     /* Sanity check for the command line */
     if (argc < 2) {
@@ -27,11 +30,26 @@ int main (int argc, void* argv[]) {
     /* Read file into memory */
     size_t n_read = fread(exe_file_data, 1, file_size, exe_file);
     if (n_read != file_size) {
-        fprintf(stderr, "reading error (%d)\n", n_read);
+        fprintf(stderr, "reading error (%lld)\n", n_read);
         return 1;
     } else {
         printf("[OK] file size = %ld byte.\n", file_size);
     }
+
+    /* Prepare the parser */
+    /* 1. DOS header (starts at the start of the file) */
+    IMAGE_DOS_HEADER* p_dos_header = (IMAGE_DOS_HEADER*) exe_file_data;
+    /* 2. NT header at the e_lfanew offset from the start of the file */
+    IMAGE_NT_HEADERS* p_nt_header = (IMAGE_NT_HEADERS*) ((char*)p_dos_header + p_dos_header->e_lfanew);
+
+    /* Print parsed information */
+    /* 1. PE compile time */
+    /* 2. PE characteristics */
+    /* 3. Address of entry point */
+    /* 4. Section info */
+    /* 5. Section locations */
+    /* 6. Parse .rsrc section and extract the content */
+    /* 7. Other essential information */
 
     fclose(exe_file);
 
